3f83b7e0ed1e98f8bcb5525b0a4645f993063c00
zhalaudien <zhalaudien@gmail.com>
2025-02-20 20:35:34 +0700

    update scrip install php minor 17

--- install_php.sh
+++ install_php.sh
@@ -58,6 +58,7 @@ EOF
 
 # Tambahkan konfigurasi PHP-FPM di httpd.conf jika belum ada
 sudo cp config/httpd.conf /etc/httpd/conf/httpd.conf
+sudo cp config/phpmyadmin.conf /etc/httpd/conf/extra/
 
 # Jalankan dan aktifkan PHP-FPM
 echo "🔄 Mengaktifkan dan memulai PHP-FPM..."
